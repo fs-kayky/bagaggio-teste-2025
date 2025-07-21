9c2e043bc2c2b99df1fa2230a8bc53822e41b353
fs-kayky <kaykyr.dev@gmail.com>
2025-07-21 14:40:59 -0300

    feat: initial commit

---
+++ src/test/java/com/fs_kayky/bagaggio_teste/BagaggioTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs_kayky.bagaggio_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagaggioTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
